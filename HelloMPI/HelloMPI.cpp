c3a017904dd2aa75428cf553b0fe11fb082eb9d8
Александр Маркелов <markelov91@gmail.com>
2022-04-05 20:19:40 +0300

    6->

--- HelloMPI/HelloMPI.cpp
+++ HelloMPI/HelloMPI.cpp
@@ -19,11 +19,10 @@ int main(int argc, char** argv) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	printf("Rank: %d", rank);
-
 	srand(12);
 
 	if (rank == 0) {
+		printf("Rank: %d\n", rank);
 		// input random nums from 0 to 2000 
 		for (int i = 0; i < N; i++) {
 			A[i] = rand() % 2001;
@@ -31,7 +30,6 @@ int main(int argc, char** argv) {
 			C[i] = rand() % 2001;
 		}
 
-		for (int cycles = 0; cycles < 10000; cycles++) {
 			startTime = MPI_Wtime();
 			for (int i = 1; i < size; i++) {
 				MPI_Send(&A[N * i / size], N / size, MPI_INTEGER, i, 0, MPI_COMM_WORLD);
@@ -45,15 +43,21 @@ int main(int argc, char** argv) {
 			for (int i = 0; i < size; i++) {
 				MPI_Recv(&Y[N * i / size], N / size, MPI_INTEGER, i, 0, MPI_COMM_WORLD, &stat);
 			}
-		}
 		endTime = MPI_Wtime();
+
+		// output
+		printf("Y = [");
+		for (int i = 0; i < N; i++)
+			printf("%d ", Y[i]);
+		printf("]\n");
+
 
 	}
 	else {
+		printf("Rank: %d\n", rank);
 		//[N * rank / size, N * (rank + 1)/size - 1]
 		// computing
 		// Yi = Ai * S1 + Ci/(Ai+Bi)
-		for (int cycles = 0; cycles < 10000; cycles++) {
 			MPI_Recv(&A[N * rank / size], N / size, MPI_INTEGER, rank, 0, MPI_COMM_WORLD, &stat);
 			MPI_Recv(&B[N * rank / size], N / size, MPI_INTEGER, rank, 1, MPI_COMM_WORLD, &stat);
 			MPI_Recv(&C[N * rank / size], N / size, MPI_INTEGER, rank, 2, MPI_COMM_WORLD, &stat);
@@ -62,14 +66,8 @@ int main(int argc, char** argv) {
 				Y[i] = A[i] * S1 + C[i] / (A[i] + B[i]);
 
 			MPI_Send(&Y[N * rank / size], N / size, MPI_INTEGER, rank, 0, MPI_COMM_WORLD);
-		}
+	
 	}
-
-	// output
-	printf("Y = [");
-	for (int i = 0; i < N; i++)
-		printf("%d ", Y[i]);
-	printf("]\n");
 
 	MPI_Finalize;
 }
